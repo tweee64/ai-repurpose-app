389cae30ed8474dc2905373ad999085db7897015
Thu Thuy Pham <phamthuthuy498@gmail.com>
2026-06-08 14:55:39 +0800

    :hammer: fix for build

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import { authConfig } from '@/app/auth.config';
 
-export const { auth: middleware } = NextAuth(authConfig);
+export const { auth: proxy } = NextAuth(authConfig);
 
 export const config = {
   matcher: ['/dashboard/:path*', '/settings/:path*'],
